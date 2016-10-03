d9d849569fd6689e0758a6db343408113483ca08
GerőcsM <GercsM@192.168.1.208>
2016-10-03 14:34:14 +0200

    language switcher activated

--- js/script.js
+++ js/script.js
@@ -392,36 +392,36 @@ $(document).ready(function () {
         $(this).tooltip('open');
     });
     //switch language
-//    $("#langswitch, #langswitch-xs").on("click", "a", function (e) {
-//        e.preventDefault();
-//        var $element = $(this);
-//        if ($element.hasClass('active')) {
-//            return false;
-//        }
-//
-//        var refresh_url = window.location.href;
-//        var href = $element.attr('href');
-//        var re = /#(.*)?$/g;
-//        var match = re.exec(refresh_url);
-//        if (match) {
-//            refresh_url = refresh_url.replace(match[0], "");
-//        }
-//
-//        var request = $.ajax({
-//            type: "POST",
-//            context: this,
-//            url: href,
-//            dataType: "json"
-//        });
-//        request.done(function (response) {
-//            if (response.status === true) {
-//                window.location.href = refresh_url;
-//            }
-//        });
-//        request.fail(function (jqXHR, exception) {
-//            displayError(jqXHR, exception);
-//        });
-//    });
+    $("#langswitch, #langswitch-xs").on("click", "a", function (e) {
+        e.preventDefault();
+        var $element = $(this);
+        if ($element.hasClass('active')) {
+            return false;
+        }
+
+        var refresh_url = window.location.href;
+        var href = $element.attr('href');
+        var re = /#(.*)?$/g;
+        var match = re.exec(refresh_url);
+        if (match) {
+            refresh_url = refresh_url.replace(match[0], "");
+        }
+
+        var request = $.ajax({
+            type: "POST",
+            context: this,
+            url: href,
+            dataType: "json"
+        });
+        request.done(function (response) {
+            if (response.status === true) {
+                window.location.href = refresh_url;
+            }
+        });
+        request.fail(function (jqXHR, exception) {
+            displayError(jqXHR, exception);
+        });
+    });
     $("#orientation_switch").on('click', '.dropdown-menu li a', function (e) {
         e.preventDefault();
         var $element = $(this);
