a13d5a9a17cc85e26f325e333a4b005d64d4f365
GerőcsM <GercsM@192.168.1.208>
2016-10-25 15:40:55 +0200

    emlam changes

--- application/views/modules-hu/emlam.php
+++ application/views/modules-hu/emlam.php
@@ -29,7 +29,7 @@
                 <table class="table table-striped">
                     <tr>
                         <td>Forrás</td>
-                        <td>A nessie.ilab.sztaki.hu-n találhatók az SRILM scriptek, illetve a modellek is.</td>
+                        <td><a href="http://sandbox.mokk.bme.hu/~ndavid/tmp/" target="_blank">http://sandbox.mokk.bme.hu/~ndavid/tmp/</a></td>
                     </tr>
                     <tr>
                         <td>Forrásnyelv</td>
@@ -37,20 +37,20 @@
                     </tr>
                     <tr>
                         <td>Input</td>
-                        <td>Soronként egy mondat, a tokenek között space. A legjobb változatnál a lemma (esetleg képzőkkel együtt) és a ragok külön tokenek.</td>
+                        <td>Soronként egy mondat, a tokenek között space (tokenizálásra használható az emToken). A legjobb változatnál a lemma (esetleg képzőkkel együtt) és a ragok külön tokenek.</td>
                     </tr>
                     <tr>
                         <td>Output</td>
-                        <td>Soronként egy mondat, a tokenek között space. A legjobb változatnál a lemma (esetleg képzőkkel együtt) és a ragok külön tokenek.</td>
+                        <td>A szövegünk valószínűsége, opcionálisan mondatokra, szavakra lebontva.</td>
                     </tr>
                     <tr>
                         <td>Futtatás</td>
                         <td>
                             Az SRILM toolkit ngram nevű programjával:
                             <br/>
-                            <span class="code">ngram -order 5 -lm lemmad_u10 -ppl &lt;szöveg file&gt;</span>                            
+                            <span class="code">ngram -order 5 -lm lemmad_u50_krs.lm5.gz -ppl &lt;szöveg file&gt;</span>                            
                             <br/>
-                            Paraméterek: -order 5: 5-grammokat használjon (jelenleg ez a legnagyobb); -lm lemmad_u10: a "gluténmentes" modellt használja, ami a 10-nél nagyobb előfordulású szavakon tanult; -ppl &lt;szöveg file&gt;: itt a mi szövegfile-unkat adjuk meg
+                            Paraméterek: -order 5: 5-grammokat használjon (jelenleg ez a legnagyobb); -lm lemmad_u50_krs.lm5.gz: a "gluténmentes" modellt használja, ami az 50-nél nagyobb előfordulású szavakon tanult; -ppl &lt;szöveg file&gt;: itt a mi szövegfile-unkat adjuk meg
                         </td>
                     </tr>
                     <tr>
